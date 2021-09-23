6ba076369580885afd45dcd672317342e08d854e
John <johnbullnz@gmail.com>
2021-09-23 16:33:17 +1200

    use nginx on host

--- setup.sh
+++ setup.sh
@@ -57,16 +57,12 @@ setup_compose() {
     REQUESTED_CHANNEL=stable
     LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
 
+    cp ./data/docker-compose.yml $REDASH_BASE_PATH
+
     cd $REDASH_BASE_PATH
-    GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-    wget https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
     sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
-    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
-    echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
-    export COMPOSE_PROJECT_NAME=redash
-    export COMPOSE_FILE=/opt/redash/docker-compose.yml
-    sudo docker-compose run --rm server create_db
-    sudo docker-compose up -d
+    docker-compose run --rm server create_db
+    docker-compose up -d
 }
 
 install_docker
